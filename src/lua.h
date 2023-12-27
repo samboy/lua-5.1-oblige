c6349bb028f522b448b6beca46dd889c23310640
Sam Trenholme <git@samiam.org>
2023-12-26 21:53:29 -0800

    Put Oblige 7.70 note when starting up this Lua fork

--- src/lua.h
+++ src/lua.h
@@ -17,7 +17,7 @@
 
 
 #define LUA_VERSION	"Lua 5.1"
-#define LUA_RELEASE	"Lua 5.1.5"
+#define LUA_RELEASE	"Lua 5.1.5 (Oblige 7.70)"
 #define LUA_VERSION_NUM	501
 #define LUA_COPYRIGHT	"Copyright (C) 1994-2012 Lua.org, PUC-Rio"
 #define LUA_AUTHORS 	"R. Ierusalimschy, L. H. de Figueiredo & W. Celes"
